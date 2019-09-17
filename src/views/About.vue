08f409675d1ce4f56e705270000c97ded52e5c19
风很大 <1832355298@qq.com>
2019-09-17 17:20:38 +0800

    000000

---
+++ src/views/About.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1 class="h">This is an about page</h1>
+  </div>
+</template>
+<style lang="scss">
+@import "../assets/css/about.scss"; //scss引入
+	.about{
+		background: #eee;
+		height: 60px;
+		line-height: 60px;
+		.h{
+			color: $color;
+			font-weight: bold;
+			font-size: 20px;
+			font: $font-stack;
+		}
+	}
+</style>
